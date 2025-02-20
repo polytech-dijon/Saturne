0dd62be0765c8555538f1531b0762c45d981cc6e
Horizon-NTH <122306936+Horizon-NTH@users.noreply.github.com>
2025-02-20 18:50:07 +0100

    Add Prisma client singleton

---
+++ __tests__/lib/prisma.test.ts
@@ -0,0 +1,19 @@
+import requireActual = jest.requireActual;
+
+const env = process.env;
+
+beforeEach(() => {
+  process.env = { ...env };
+});
+
+afterEach(() => {
+  process.env = env;
+});
+
+it('should reuse the same PrismaClient instance when imported multiple times in development', async () => {
+  (process.env.NODE_ENV as any) = 'development';
+  const prismaFirst = requireActual('../../src/lib/prisma').default;
+  const prismaSecond = requireActual('../../src/lib/prisma').default;
+
+  expect(prismaFirst).toBe(prismaSecond);
+});
