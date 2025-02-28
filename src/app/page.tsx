d2bca8105a957b0457df1b4f2354d5f3a0aa8ac9
Horizon-NTH <122306936+Horizon-NTH@users.noreply.github.com>
2025-02-28 15:43:26 +0100

    Add temporary placeholders for other elements of main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,14 @@ import PosterCarousel from '@/components/PosterCarousel';
 export default async function Home() {
   const posters = await getPosters();
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="min-h-screen flex justify-between items-center flex-col">
+      <div className="border-amber-950 border-4 w-full h-[20vh] flex items-center justify-center">Divia</div>
       <PosterCarousel posters={posters} />
+      <div className="border-amber-950 border-4 w-full h-[10vh] flex items-center justify-around">
+        <p>Logo</p>
+        <p>Time</p>
+        <p>Creator</p>
+      </div>
     </div>
   );
 }
--- src/components/PosterCarousel.tsx
+++ src/components/PosterCarousel.tsx
@@ -7,7 +7,7 @@ import Autoplay from 'embla-carousel-autoplay';
 export default function PosterCarousel({ posters }: { posters: Poster[] }) {
   return (
     <Carousel
-      className="w-full"
+      className="w-full h-full"
       opts={{
         loop: true,
         duration: 60,
@@ -20,8 +20,9 @@ export default function PosterCarousel({ posters }: { posters: Poster[] }) {
       ]}>
       <CarouselContent className="-ml-[10/3vw]">
         {posters.map((poster) => (
-          <CarouselItem key={poster.id} className="basis-1/3 pl-[5/3vw] flex items-center justify-center">
-            <Image src={poster.image} alt={poster.title} width={800} height={400} className="w-[30vw]" />
+          <CarouselItem key={poster.id} className="basis-1/3 h-full pl-[5/3vw] flex items-center justify-center">
+            <Image src={poster.image} alt={poster.title} width={800} height={400}
+                   className="max-w-[30vw] max-h-[30vh]" />
           </CarouselItem>
         ))}
       </CarouselContent>
