9fc7d1c6aa153365ac8156efa30a18f7bfd01171
Horizon-NTH <122306936+Horizon-NTH@users.noreply.github.com>
2025-02-28 16:39:39 +0100

    Refactor PosterCarousel to introduce transition duration

--- src/components/PosterCarousel.tsx
+++ src/components/PosterCarousel.tsx
@@ -5,27 +5,30 @@ import { Poster } from '@/lib/actions';
 import Autoplay from 'embla-carousel-autoplay';
 
 export default function PosterCarousel({ posters }: { posters: Poster[] }) {
-  return (
-    <Carousel
-      className="w-full h-full"
-      opts={{
-        loop: true,
-        duration: 60,
-      }}
-      plugins={[
-        Autoplay({
-          delay: 5000,
-          stopOnInteraction: false,
-        }),
-      ]}>
-      <CarouselContent className="-ml-[10/3vw]">
-        {posters.map((poster) => (
-          <CarouselItem key={poster.id} className="basis-1/3 h-full pl-[5/3vw] flex items-center justify-center">
-            <Image src={poster.image} alt={poster.title} width={800} height={400}
-                   className="max-w-[30vw] max-h-[30vh]" />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
-  );
+    const transitionDuration = 1000;
+    const posterDuration = 5000; // TODO update when each poster has its own duration
+    return (
+        <Carousel
+            className="w-full h-full"
+            opts={{
+                loop: true,
+                duration: 60,
+            }}
+            plugins={[
+                Autoplay({
+                    delay: posterDuration - transitionDuration,
+                    stopOnInteraction: false,
+                }),
+            ]}>
+            <CarouselContent className="-ml-[10/3vw]">
+                {posters.map((poster) => (
+                    <CarouselItem key={poster.id}
+                                  className="basis-1/3 h-full pl-[5/3vw] flex items-center justify-center">
+                        <Image src={poster.image} alt={poster.title} width={800} height={400}
+                               className="max-w-[30vw] max-h-[30vh]" />
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+        </Carousel>
+    );
 }
